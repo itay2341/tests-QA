cf7ca349b49beadeeab9a290fbe055bcd2db118d
Ze'ev Ben-Tsvi <zeevbts@gmail.com>
2023-11-26 16:31:14 +0200

    Commit Zeev 2 again

--- zeev.py
+++ zeev.py
@@ -3,6 +3,10 @@ print("slow but increasing")
 
 print("not Zeev")
 
-
+print("itay!!!")
+print("itay!!!")
+print("itay!!!")
+print("itay!!!")
+print("itay!!!")
 print("zeev")
 print("slow but increasing")
